be76e0b992abd1a824acda02f1616e5940d5eaf0
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2019-10-30 11:08:05 +0000

    BLD: Release 4.14 build

--- twine-upload.bat
+++ twine-upload.bat
@@ -1,10 +1,10 @@
 if "%TESTPYPI_UPLOAD%" == "true" (
-    twine upload --skip-existing --repository-url https://test.pypi.org/legacy/ -u %TESTPYPI_USERNAME% -p %TESTPYPI_PASSWORD% arch/dist/*
+    twine upload --skip-existing --repository-url https://test.pypi.org/legacy/ -u %TESTPYPI_USERNAME% -p %TESTPYPI_PASSWORD% linearmodels/dist/*
 ) else (
     echo "Test PyPi upload is disabled"
 )
 if "%PYPI_UPLOAD%" == "true" (
-    twine upload --skip-existing -u %PYPI_USERNAME% -p %PYPI_PASSWORD% arch/dist/*
+    twine upload --skip-existing -u %PYPI_USERNAME% -p %PYPI_PASSWORD% linearmodels/dist/*
 ) else (
     echo "PyPi upload is disabled"
 )
