fbf88a8334b8b04b070112442ec4afdcabe9443a
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2021-01-05 18:08:30 +0000

    Yet another try

--- twine-upload.sh
+++ twine-upload.sh
@@ -10,14 +10,14 @@ if [[ ${TESTPYPI_UPLOAD} == true  || ${PYPI_UPLOAD} == true ]]; then
     pip install twine
     echo ${TRAVIS_BUILD_DIR}/wheelhouse/
     ls ${TRAVIS_BUILD_DIR}/wheelhouse/
-    if [[ ${TESTPYPI_UPLOAD} == true ]] && [[ -z ${UPLOAD_USERNAME} ]] && [[ -z ${UPLOAD_PASSWORD} ]]; then
-          twine upload --skip-existing --repository-url https://test.pypi.org/legacy/ -u $UPLOAD_USERNAME -p $UPLOAD_PASSWORD ${TRAVIS_BUILD_DIR}/wheelhouse/*.whl
+    if [[ ${TESTPYPI_UPLOAD} == true ]]; then
+          twine upload --skip-existing --repository-url https://test.pypi.org/legacy/ -u ${TESTPYPI_USERNAME} -p ${TESTPYPI_PASSWORD} ${TRAVIS_BUILD_DIR}/wheelhouse/*.whl
     else
         echo "Test PyPi upload is disabled"
     fi
     
-    if [[ ${PYPI_UPLOAD} == true ]] && [[ -z ${UPLOAD_USERNAME} ]] && [[ -z ${UPLOAD_PASSWORD} ]]; then
-        twine upload --skip-existing -u $UPLOAD_USERNAME -p $UPLOAD_PASSWORD ${TRAVIS_BUILD_DIR}/wheelhouse/*.whl
+    if [[ ${PYPI_UPLOAD} == true ]]; then
+        twine upload --skip-existing -u ${PYPI_USERNAME} -p ${PYPI_PASSWORD} ${TRAVIS_BUILD_DIR}/wheelhouse/*.whl
     else
         echo "PyPi upload is disabled"
     fi
