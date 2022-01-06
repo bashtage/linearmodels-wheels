54dd11e2fd5bfb357cde5fc9ba45314c76f294d8
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2022-01-06 13:02:49 +0000

    ENH: Finalize steps

--- config.sh
+++
@@ -1,5 +0,0 @@
-function run_tests {
-    # Runs tests on installed distribution from an empty directory
-    python --version
-    MPLBACKEND="agg" python -c "import linearmodels; linearmodels.test(['-n','auto','--skip-slow','--skip-smoke'])"
-}
